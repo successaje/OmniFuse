11a6c0ae4100aa16f0f6d04ff79c0f3369cb9223
Sukuna <ajeemmanuel221@gmail.com>
2025-08-19 17:48:36 +0100

    added the contract context

---
+++ Context/constants.js
@@ -0,0 +1,8 @@
+import omnivault from "./Omnivault.json"
+import omniexecutor from "./Omniexecutor.json"
+
+export const OmniVaultContractAddress = "0x49fd2BE640DB2910c2fAb69bB8531Ab6E76127ff"
+export const OmniExecutorContractAddress = "0x4631BCAbD6dF18D94796344963cB60d44a4136b6"
+
+export const OmniVaultABI = omnivault.abi;
+export const OmniExecutorABI = omniexecutor.abi;
